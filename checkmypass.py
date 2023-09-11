8afb9cc611ba750f3806e737d88e83124ff552de
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-11 11:54:55 +0300

    Password Checker 2

--- checkmypass.py
+++ checkmypass.py
@@ -6,11 +6,19 @@ def request_api_data(query_char):
 	res = requests.get(url)
 	if res.status_code != 200 :
 		raise RuntimeError(f'Error fetching: {res.status_code},check the api and try again')
+	return res
+
+def get_password_leaks_count(hashes,hash_to_check):
+	hashes = (line.split(':') for line in hashes.text.splitlines())
+	for h,count in hashes:
+		print(h,count)
 
 
 def pwned_api_check(password):
-	sha1password = hashlib.sha1(password.encode('utf-8')).hexgigest().upper()
-	return sha1password
+	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+	first5_char,tail = sha1password[:5],sha1password[5:]
+	response = request_api_data(first5_char)
+	return get_password_leaks_count(response,tail)
 
 
-request_api_data('CBFDA')
+pwned_api_check('CBFDA')
